d6d472dca013e4dab4b3eb70bbf774565665ad8f
MJR <m.j.rahman@outlook.com>
2022-07-07 21:12:25 +0100

    rev

---
+++ reverser.js
@@ -0,0 +1,16 @@
+function reverse(x) {
+    if (typeof x === "string") {
+        let res = ""
+        for (let i = x.length - 1; i >= 0; i--) {
+            res += x[i]
+        }
+        return res
+    }
+    if (Array.isArray(x)) {
+        let res = []
+        for (let i = x.length - 1; i>= 0; i--) {
+            result.push(x[i])
+        }
+        return res
+    }
+}
