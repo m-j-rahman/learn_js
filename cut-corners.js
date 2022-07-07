7519fc1981e428f9590303d74e1d0f8e67854b3b
MJR <m.j.rahman@outlook.com>
2022-07-07 19:09:22 +0100

    mod

--- cut-corners.js
+++ cut-corners.js
@@ -1,6 +1,53 @@
+function multiply(a, b) {
+    let num = 0;
+    if (b > 0) {
+        for (let i = 0; i < b; i++) {
+            num += a;
+        }
+    } else {
+        for (let i = b; i < 0; i++) {
+            num -= a;
+        }
+    }
+    return num
+}
+
+function divide(a, b) {
+    let res = 0;
+    if (b > 0) {
+        if (a > 0) {
+            while (a - b > 0) {
+                res++;
+                a -= b;
+            }
+        } else {
+            while (a + b < 0) {
+                res--;
+                a += b;
+            }
+        }
+    } else {
+        if (a > 0) {
+            while (a + b > 0) {
+                a += b;
+                res--;
+            }
+        } else {
+            while (a - b < 0) {
+                a -= b;
+                res++;
+            }
+        }
+    }
+    return res;
+}
+
+function modulo(a, b) {
+    return a - multiply(divide(a, b), b);
+}
+
 function trunc(num) {
-    let i = (num * 1) ^ num.length;
-    return i;
+    return num - modulo(num, 1)
 }
 
 function floor(num) {
