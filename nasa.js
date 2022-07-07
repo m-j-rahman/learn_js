3ca887bb48e26dadc8c061f58d06af004c16559f
MJR <m.j.rahman@outlook.com>
2022-07-07 22:15:30 +0100

    nasa

--- nasa.js
+++ nasa.js
@@ -3,7 +3,7 @@ function nasa(N) {
     for (let i = 1; i <= N; i++) {
         if (i % 3 === 0) res += "NA"
         if (i % 5 === 0) res += "SA"
-        if (!(i % 3 === 0)) res += i
+        else if (!(i % 3 === 0)) res += i
         if (i !== N) res += " "
     }
     return res
