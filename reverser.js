c32635cad9484ec7bdebcd7c8d3a96992a919e68
MJR <m.j.rahman@outlook.com>
2022-07-07 21:12:44 +0100

    rev

--- reverser.js
+++ reverser.js
@@ -9,7 +9,7 @@ function reverse(x) {
     if (Array.isArray(x)) {
         let res = []
         for (let i = x.length - 1; i>= 0; i--) {
-            result.push(x[i])
+            res.push(x[i])
         }
         return res
     }
