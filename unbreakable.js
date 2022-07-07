0b004d2c10d0e04a42dfc0df14610df1a2f32e95
MJR <m.j.rahman@outlook.com>
2022-07-07 22:00:34 +0100

    join

--- unbreakable.js
+++ unbreakable.js
@@ -1,25 +1,25 @@
 function split(str, elem) {
-    let arr = [];
-    for (let i = 0; i < str.length - elem.length + 1; i++) {
-        if (str.slice(i, i + elem.length) === elem) {
-            arr.push(str.slice(0, i));
-            str = str.slice(i + elem.length);
-            i = 0
-        }
+    let words = [str];
+    if (elem === "") return Array.from(str)
+    if (elem === undefined) return Array.from(str)
+    str += elem
+    while (true) {
+        let index = str.indexOf(elem)
+        if (index === -1) break
+        words.push(str.slice(0, index))
+        str = str.slice(index + elem.length)
     }
-    arr.push(str);
-    return arr;
+    if (words.length > 1) words.shift()
+    return words
 }
 
-function join(arr, elem) {
-    let str = "";
-    for (let i = 0; i < arr.length; i++) {
-        if (elem.length === 0) {
-            str += arr[i];
-        } else {
-            str += arr[i] + elem
-        } 
+function join(arr, elem = ',') {
+    let res = "";
+    for (const index in arr) {
+        res += arr[index]
+        if (index < arr.length - 1) {
+            res += sep
+        }
     }
-    str = str.slice(0, str.length - elem.length)
-    return str
+    return res
 }
