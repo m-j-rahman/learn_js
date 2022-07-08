4fcf571f259bf107c5e1c035089da68ca95d3261
MJR <m.j.rahman@outlook.com>
2022-07-08 14:17:39 +0100

    pyr

--- pyramid.js
+++ pyramid.js
@@ -1,7 +1,7 @@
 function pyramid(str, depth) {
     let result = ""
     for (let i = 1; i <= depth; i++) {
-        result += " ".repeat((depth - i) + str.length)
+        result += " ".repeat((depth - i) * str.length)
         result += str.repeat(2 * i - 1)
         if (i !== depth) result += "\n"
     }
