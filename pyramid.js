83130cb01e12e22b427504c657aaf83c45f685ca
MJR <m.j.rahman@outlook.com>
2022-07-08 15:09:42 +0100

    regexp

--- pyramid.js
+++ pyramid.js
@@ -2,8 +2,9 @@ function pyramid(str, depth) {
     let result = ""
     for (let i = 1; i <= depth; i++) {
         result += " ".repeat((depth - i) * str.length)
-        result += str.repeat(2 * i - 1)
+        // result += str.repeat(2 * i - 1)
         if (i !== depth) result += "\n"
     }
     return result
 }
+console.log(pyramid('hi', 6))
