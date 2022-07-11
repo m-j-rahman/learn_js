46d8959864e2c8cd47b108199f3cb88b994f6ce3
MJR <m.j.rahman@outlook.com>
2022-07-11 16:51:49 +0100

    dates

---
+++ date-is.js
@@ -0,0 +1,23 @@
+function isValid(date) {
+    if (isNaN(date) || date === 0) {
+        return false
+    } else {
+        return true
+    }
+}
+
+function isAfter(date1, date2) {
+    return date1 > date2
+}
+
+function isBefore (date1, date2) {
+    return date1 < date2
+}
+
+function isFuture(date) {
+    return date > Date.now
+}
+
+function isPast(date) {
+    return date < Date.now
+}
