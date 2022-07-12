f0f3def2d4bce10bef5cfd229d6929f652e3285b
MJR <m.j.rahman@outlook.com>
2022-07-12 13:34:28 +0100

    return

--- match-cron.js
+++ match-cron.js
@@ -1,30 +1,30 @@
 function matchCron(cron, date) {
-    let splitcron = cron.split(" ")
-    for(let i = 0; i < cronArray.length; i++) {
-        if (cronArray[i] != "*") {
+    let cronSplit = cron.split(" ")
+    for(let i = 0; i < cronSplit.length; i++) {
+        if (cronSplit[i] != "*") {
             switch (i) {
                 case 0:
-                    if (date.getMinutes() != splitcron[i]) {
+                    if (date.getMinutes() != cronSplit[i]) {
                         return false
                     }
                     break
                 case 1:
-                    if (date.getHours() != splitcron[i]) {
+                    if (date.getHours() != cronSplit[i]) {
                         return false
                     }
                     break
                 case 2:
-                    if (date.getDate() != splitcron[i]) {
+                    if (date.getDate() != cronSplit[i]) {
                         return false
                     }
                     break
                 case 3:
-                    if (date.getMonth() != splitcron[i]-1) {
+                    if (date.getMonth() != cronSplit[i]-1) {
                         return false
                     }
                     break
                 case 4:
-                    if (date.getDay() != splitcron[i]) {
+                    if (date.getDay() != cronSplit[i]) {
                         return false
                     }
             }
