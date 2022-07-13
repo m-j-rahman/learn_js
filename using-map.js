d9a0c9b44e01b1c5f134ac0cd3e90a12d8c258f1
MJR <m.j.rahman@outlook.com>
2022-07-13 15:36:33 +0100

    temp

---
+++ using-map.js
@@ -0,0 +1,27 @@
+function citiesOnly(arr) {
+    return arr.map(elem => elem.city) 
+}
+
+function upperCasingStates(arr) {
+    return arr.map(state => {
+        return state.split(" ").map(word => {
+            return word.char(0).toUpperCase() + word.slice(1)
+        }).join(" ")
+    })
+}
+
+function fahrenheitToCelsius(arr) {
+    return arr.map((elem) => Math.floor((parseInt(elem)-32)/1.8) + "°C")
+}
+
+function trimTemp(arr) {
+    return arr.map(elem => {
+        elem["temperature"] = elem["temperature"].split(" ").join("")
+        return elem
+    })
+}
+
+function tempForecasts(arr) {
+    arr = trimTemp(arr)
+    return arr.map((elem) => fahrenheitToCelsius([trimTemp([elem])[0].temperature]) + "elsius in " + upperCasingStates([e.city, e.state]).join(", "))
+}
