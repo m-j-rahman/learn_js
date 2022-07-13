09a4d6d7ff39f80ad3b4c81af0e2c5c210b2682b
MJR <m.j.rahman@outlook.com>
2022-07-13 20:58:22 +0100

    reduce

--- reduce.js
+++ reduce.js
@@ -1,35 +1,29 @@
 function fold(arr, func, acc = 0) {
     for (let i = 0; i < arr.length; i++) {
-        acc = func(acc, arr[i])
+        acc = func(acc, arr[i], arr)
     }
     return acc
 }
 
 function foldRight(arr, func, acc = 0) {
     for (let i = arr.length-1; i >= 0; i--) {
-        acc = func(acc, arr[i])
+        acc = func(acc, arr[i], arr)
     }
     return acc
 }
 
 function reduce(arr, func) {
-    if (arr.length < 1) {
-        return null
-    }
     let acc = arr[0]
     for (let i = 1; i < arr.length; i++) {
-        acc = func(acc, arr[i])
+        acc = func(acc, arr[i], arr)
     }
     return acc
 }
 
 function reduceRight(arr, func) {
-    if (arr.length < 1) {
-        return null
-    }
     let acc = arr[arr.length-1]
     for (let i = arr.length-2; i >= 0; i--) {
-        acc = func(acc, arr[i])
+        acc = func(acc, arr[i], arr)
     }
     return acc
 }
