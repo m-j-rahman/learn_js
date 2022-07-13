91e6064736e87174717b4fd7d9c1d0a6f4959368
MJR <m.j.rahman@outlook.com>
2022-07-13 20:38:27 +0100

    part

--- filter.js
+++ filter.js
@@ -5,4 +5,27 @@ function filter(arr, func) {
             res.push(arr[i])
         }
     }
+    return res
+}
+
+function reject(arr, func) {
+    let res = []
+    for (let i = 0; i < arr.length; i++) {
+        if (func(arr[i], i, arr) != true) {
+            res.push(arr[i])
+        }
+    }
+    return res
+}
+
+function partition(arr, func) {
+    let res = [[],[]]
+    for (let i = 0; i < arr.length; i++) {
+        if (func(arr[i], i, arr) == true) {
+            res[0].push(arr[i])
+        } else {
+            res[1].push(arr[i])
+        }
+    }
+    return res
 }
