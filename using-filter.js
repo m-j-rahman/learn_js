e8a22f733e87035bceddb1f72cc801e95aca6809
MJR <m.j.rahman@outlook.com>
2022-07-13 16:17:59 +0100

    multi

---
+++ using-filter.js
@@ -0,0 +1,28 @@
+function filterShortStateName(arr) {
+    return arr.filter((str) => str.length < 7)
+}
+
+function filterStartVowel(arr) {
+    return arr.filter((str) => str.match(/^[aeiou]/i))
+}
+
+function filter5Vowels(arr) {
+    return arr.filter((str) => str.match(/^[aeiou]/ig).length >= 5)
+}
+
+function filter1DistinctVowel(arr) {
+    return arr.filter((str) => {
+        let vowels = filterStartVowel(str.toLowerCase().split(""))
+        vowels.sort()
+        return vowels[0] === vowels[vowels.length - 1]
+    })
+}
+
+function multiFilter(arr) {
+    return arr.filter((obj) => {
+        obj.capital.length > 7 &&
+        !obj.name.match(/^[aeiou]/i) &&
+        obj.tag.match(/^[aeiou]/ig) && 
+        obj.region !== "South"
+    })
+}
