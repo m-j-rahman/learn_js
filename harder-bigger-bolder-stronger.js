2b27d294ef1c694a38ce614d4eb75866d7646a3e
MJR <m.j.rahman@outlook.com>
2022-07-14 21:24:40 +0100

    size

--- harder-bigger-bolder-stronger.js
+++ harder-bigger-bolder-stronger.js
@@ -2,7 +2,7 @@ export function generateLetters() {
     const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     let size = 11, letters = 120, weight = 300;
     let partition = (letters / 3);
-    let section = partition - 1;
+    let partCount = partition - 1;
     for (let i = 0; i < letters; i++) {
         let letter = alphabet[Math.floor(Math.random() * alphabet.length)];
         const newDiv = document.createElement("div");
@@ -11,8 +11,8 @@ export function generateLetters() {
         newDiv.style.fontSize = String(size) + "px";
         newDiv.style.fontWeight = String(weight)
         document.body.append(newDiv);
-        if (i === section) {
-            section += partition
+        if (i === partCount) {
+            partCount += partition
             if (weight === 300) {
                 weight += 100
             } else {
