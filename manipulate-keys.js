c71224ebe8055d5bf1d72bcaa1dccd75d9af050d
MJR <m.j.rahman@outlook.com>
2022-07-20 16:08:02 +0100

    val

---
+++ manipulate-keys.js
@@ -0,0 +1,31 @@
+function filterKeys(item, func) {
+    let result = {}
+    for (let value of Object.entries(item)){
+        if(func(value[0])) { 
+            result[value[0]] = value[1]
+        }
+    }
+    return result
+}
+
+function mapKeys(item, func) {
+    let result = {}
+    for (let value of Object.entries(item)){
+            result[func(value[0])] = value[1]
+    }
+    return result
+}
+
+function reduceKeys(item, func, start = 0) {
+    let accumulator = start
+    let count = 0
+    for (let value of Object.entries(item)){
+        if(count == 0) {
+            accumulator = value[0]
+        } else { 
+            accumulator = func(accumulator,value[0])
+        }
+        count++
+    }    
+    return accumulator
+}
