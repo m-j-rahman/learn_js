aae4e82330c3a6a6cfa935f80fc27a3446dd4d99
MJR <m.j.rahman@outlook.com>
2022-07-20 15:00:35 +0100

    omit

--- pick-omit.js
+++ pick-omit.js
@@ -1,21 +1,23 @@
 function pick(obj, keys) {
-    const result = {}
-    let keysObj = Object.keys(obj)
-    keysObj.forEach(key => {
-        if (key == keys) {
-            result[key] = obj[key]
-        }
-    })
-    return result
-}
+    const result = {};
+    keys instanceof Array ? (keys = keys) : (keys = new Array(keys));
+    for (let each of keys) {
+      if (obj.hasOwnProperty(each)) {
+        result[each] = obj[each];
+      }
+    }
+    return result;
+  };
 
 function omit(obj, keys) {
-    const result = {}
-    let keysObj = Object.keys(obj)
-    keysObj.forEach(key => {
-        if (key != keys) {
-            result[key] = obj[key]
-        }
-    })
-    return result
-}
+    const result = {
+      ...obj,
+    };
+    keys instanceof Array ? (keys = keys) : (keys = new Array(keys));
+    for (let each of keys) {
+      if (obj.hasOwnProperty(each)) {
+        delete result[each];
+      }
+    }
+    return result;
+  };
